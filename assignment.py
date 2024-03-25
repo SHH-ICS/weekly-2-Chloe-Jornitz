202a4323253c8684d0e2b51a2dea86013ead6eee
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-03-25 13:30:21 +0000

    add code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,20 @@
-
+import math
+print ("Welcome!")
+print ("Calculate the Area and Circumference of a Circle")
+print ("What is the diameter of the circle?")
+diameter = input()
+if diameter.isdigit == True:
+  d = float(diameter)
+  if d < 0:
+    print ("Error")
+    print ("The diameter you input is a negative number.")
+    print ("Make sure to only input positive numbers.")
+  else:
+    r = float(d/2)
+    print ("Result:")
+    print ("The area of the circle is:")
+    print (r**2*math.pi)
+    print ("The circumference of the circle is:")
+    print (d*math.pi)
+else: 
+  print ("Error")
