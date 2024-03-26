ca4d9de17c35576e718866086001c4f8bba54358
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-03-26 13:09:50 +0000

    loop

--- assignment.py
+++ assignment.py
@@ -1,17 +1,20 @@
-import math
-print ("Welcome!")
-print ("Calculate the Area and Circumference of a Circle")
-print ("What is the diameter of the circle?")
-diameter = input()
-if diameter.isnumeric() == True:
-  d = float(diameter)
-  r = float(d/2)
-  print ("Result:")
-  print ("The area of the circle is:")
-  print (r**2*math.pi)
-  print ("The circumference of the circle is:")
-  print (d*math.pi)
-else: 
-  print ("Error")
-  print ("The diameter you input was either negative or not a number.")
-  print ("Make sure to only include positive numbers and not to include letters or units.")
+while 1:
+  import math
+  print ("Welcome!")
+  print ("Calculate the Area and Circumference of a Circle")
+  print ("What is the diameter of the circle?")
+  diameter = input()
+  if diameter.isnumeric() == True:
+    d = float(diameter)
+    r = float(d/2)
+    print ("Result:")
+    print ("The area of the circle is:")
+    print (r**2*math.pi)
+    print ("The circumference of the circle is:")
+    print (d*math.pi)
+    break
+  else: 
+    print ("Error")
+    print ("The diameter you input was either negative or not a number.")
+    print ("Try again and make sure to only include positive numbers and not to include letters or units.")
+    continue
