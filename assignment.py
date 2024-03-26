9437dc26dbd11a91995bea687516b4cf79c56657
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-03-26 13:23:15 +0000

    decimal

--- assignment.py
+++ assignment.py
@@ -1,10 +1,10 @@
+import math
+print ("Welcome!")
+print ("Calculate the Area and Circumference of a Circle")
 while 1:
-  import math
-  print ("Welcome!")
-  print ("Calculate the Area and Circumference of a Circle")
   print ("What is the diameter of the circle?")
   diameter = input()
-  if diameter.isnumeric() == True:
+  if diameter.isdecimal() == True:
     d = float(diameter)
     r = float(d/2)
     print ("Result:")
@@ -14,7 +14,7 @@ while 1:
     print (d*math.pi)
     break
   else: 
-    print ("Error")
+    print ("Error!")
     print ("The diameter you input was either negative or not a number.")
     print ("Try again and make sure to only include positive numbers and not to include letters or units.")
     continue
