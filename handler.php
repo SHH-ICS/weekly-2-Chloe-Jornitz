cbf430518385319718c1fe005a3b6e56d40c839c
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-03-30 20:41:18 +0000

    edit

--- handler.php
+++ handler.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
+    <title>Results</title>
   </head>
 
   <body>
@@ -16,12 +16,27 @@
     }
     try {
       settype($diameter, "float");
+      if ($diameter >= 0)
+      {
       $radius = diameter/2;
       $area = M_PI*radius**2;
       $circumference = M_PI*diameter;
+      echo "Results";
+      echo "The area of the circle is:";
+      echo $area;
+      echo "The circumference of the circle is:";
+      echo $circumference;
+      }
+      else {
+        echo "Error!";
+        echo "The diameter you input was either negative or not a number.";
+        echo "Try again and make sure to only include positive numbers and not to include letters or units.";
+      }
     }
     catch (exception $letter) {
-
+      echo "Error!";
+      echo "The diameter you input was either negative or not a number.";
+      echo "Try again and make sure to only include positive numbers and not to include letters or units.";
     }
     ?>
     
